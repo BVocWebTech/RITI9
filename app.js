9905358679a0fa81c58f929f4e077a50b7118c7d
BVocWebTech <162856839+BVocWebTech@users.noreply.github.com>
2024-08-11 09:56:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -112,7 +112,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+document.getElementById("read_more").addEventListener( 'click' , changeClass);
 
+function changeClass() {
+  var content = document.getElementById("extra_content");
+  var btn = document.getElementById("read_more");
+  content.classList.toggle('show');
+
+  if (content.classList.contains("show")) {
+      btn.innerHTML = "Show Less";
+  } else {
+      btn.innerHTML = "Show More";
+  }
+}
 
 
 
