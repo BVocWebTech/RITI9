827f0dc29ab53492d673f84809cbd3963ce57dfb
BVocWebTech <162856839+BVocWebTech@users.noreply.github.com>
2024-08-11 16:45:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -181,3 +181,9 @@ document.addEventListener('DOMContentLoaded', () => {
         eventSection.style.display = 'block';
     });
 });
+function showMore(){
+    //removes the link
+    document.getElementById('link').parentElement.removeChild('link');
+    //shows the #more
+    document.getElementById('more').style.display = "block";
+}
